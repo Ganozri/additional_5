8d8c020c329b8bb5cdfc9e22395d190cec9bf7bb
Ganozri <Ganozri@users.noreply.github.com>
2018-02-22 18:56:11 +0300

    "solution"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function solveEquation(equation) 
+{
+var aa="";
+var bb="";
+var cc="";
+var d;
+var x1=undefined;
+var x2=undefined;
+
+var damn = equation.split('*');
+var a = damn[0];
+aa = parseFloat(a);
+
+var b = damn[1];
+b=b.split('');
+b=b.splice(5,b.length-1);
+b.pop();
+for(var i = 0;i<b.length;i++)
+{
+if(i!=1)
+	{
+bb+=b[i];
+	}
+}
+bb = parseFloat(bb);
+
+var c = damn[2];
+c=c.split('');
+c=c.splice(5,c.length-1);
+for(var i = 0;i<c.length;i++)
+{
+cc+=c[i];	
+}
+cc = parseFloat(cc);
+
+d=bb*bb-4*aa*cc;
+if(d>0)
+{
+ x2=-(bb-Math.sqrt(d))/(2*aa);
+ x1=-(bb+Math.sqrt(d))/(2*aa);
+}
+if(d=0)
+{
+x2=bb/(2*aa);
+x1=x1;
+}
+if(d<0)
+{
+x1=x2=undefined;
+}
+if(x1>x2)
+{
+var solutions=[x2,x1];
+}
+else
+{
+var solutions=[x1,x2];
+}
+return solutions;
+
 }
