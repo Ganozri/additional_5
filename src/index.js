aca2852c3731029cd7712c2fa8629d2b80ab5935
Ganozri <Ganozri@users.noreply.github.com>
2018-02-27 00:58:14 +0300

    "solution"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+// your solution
+var stack = [];
+var check,ch=0;
+for (i=0; i<str.length;i++) {
+c=str[i];
+if (c == '{' || c == '[' || c == '(' ) {
+stack.push(c);
+} else
+if (c=='|' && ch==0) {stack.push(c);ch++;}else {
+if (stack) {
+check = stack.pop();
+if (c == '}' && check != '{') {
+return false;
+} else if (c == ']' && check != '[') {
+return false;
+} else if (c == ')' && check != '(') {
+return false;}
+else if (c == '|' && check != '|') {
+return false;}
+else if (c == '|' && check == '|') {
+ch=0;}
+
+
+} else {
+return false;
+}
+}
+}
+
+if ( stack.pop() ) return false;
+
+return true;
 }
